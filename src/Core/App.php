584d46819017d5b151737fab330de54e02b79766
KunYuChang <mischiefsub@gmail.com>
2023-08-01 20:37:10 +0800

    創建Router類別

--- src/Core/App.php
+++ src/Core/App.php
@@ -1,11 +1,20 @@
 <?php
 
+// 設計為管理框架所有的工具。
+
 declare(strict_types=1);
 
 namespace Core;
 
 class App
 {
+    private Router $router;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+    }
+
     public function run()
     {
         echo "Application is running";
