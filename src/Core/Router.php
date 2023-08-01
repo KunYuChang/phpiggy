d4a8b41abda73f511cb1ceaf0ab3fd85d35bcfee
KunYuChang <mischiefsub@gmail.com>
2023-08-01 23:47:41 +0800

    Normalizing Paths

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -10,9 +10,17 @@ class Router
 
     public function add(string $method, string $path)
     {
+        $path = $this->normalizePath($path);
         $this->routes[] = [
             'method' => strtoupper($method),
             'path' => $path
         ];
+    }
+
+    public function normalizePath(string $path): string
+    {
+        $path = trim($path, '/');
+        $path = "/{$path}/";
+        return preg_replace('#/{2,}#', '/', $path); // 連續出現2個以上的/就替換成1個/
     }
 }
