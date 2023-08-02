48a34bd45f2ef173ad2acac14925a64a2487552d
KunYuChang <mischiefsub@gmail.com>
2023-08-02 20:48:23 +0800

    使用 Class Magic Constant

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -8,10 +8,12 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Core\App;
+use App\Controllers\HomeController;
 
 $app = new App();
 
 // add router (contain the name of the class and method in the array)
-$app->get('/', ['App\Controllers\HomeController',]); // root url
+// PHP 8 可以用HomeController::class取代'App\Controllers\HomeController'
+$app->get('/', [HomeController::class, 'home']); // root url
 
 return $app;
