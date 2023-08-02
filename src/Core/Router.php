0823416978830bbf472a42a7e3d78f072fc19f6b
KunYuChang <mischiefsub@gmail.com>
2023-08-02 22:32:59 +0800

    確認 Request 是否在設定的路由當中

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -31,6 +31,16 @@ class Router
         $path = $this->normalizePath($path);
         $method = strtoupper($method);
 
-        echo $path . $method;
+        foreach ($this->routes as $route) {
+            // Guard Condition : return first
+            if (
+                !preg_match("#^{$route['path']}$#", $path) ||
+                $route['method'] !== $method
+            ) {
+                continue;
+            }
+
+            echo 'route found';
+        }
     }
 }
