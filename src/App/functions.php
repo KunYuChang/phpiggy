ca73bc8e0c78ee2a22347c846c2a14a843ff28d3
KunYuChang <mischiefsub@gmail.com>
2023-08-13 15:10:05 +0800

    Escaping Data

--- src/App/functions.php
+++ src/App/functions.php
@@ -2,11 +2,17 @@
 
 declare(strict_types=1);
 
-
+// dump and die
 function dd(mixed $value)
 {
     echo "<pre>";
     var_dump($value);
     echo "</pre>";
     exit;
+}
+
+// escape
+function e(mixed $value): string
+{
+    return htmlspecialchars((string) $value);
 }
