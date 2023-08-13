0f1879c14e520ed23e4e29c956ec643f27dc5215
KunYuChang <mischiefsub@gmail.com>
2023-08-13 09:35:07 +0800

    Extracting Arrays Extracting os optional but makes our templates more readable

--- src/Core/TemplateEngine.php
+++ src/Core/TemplateEngine.php
@@ -11,8 +11,11 @@ class TemplateEngine
     ) {
     }
 
-    public function render(string $template)
+    public function render(string $template, array $data = [])
     {
+        // extract 可以依據陣列中的 key 創建變數。
+        // EXTR_SKIP 代表如果 extract 創建的變數名稱與原本的存在的變數相同時，則忽略不創建。
+        extract($data, EXTR_SKIP);
         include "{$this->basePath}/{$template}";
     }
 }
