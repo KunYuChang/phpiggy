4df12b587473d3010ee026bcd61bb82017bc0828
KunYuChang <mischiefsub@gmail.com>
2023-08-14 20:17:55 +0800

    新增註冊表單並設定routes、controller

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Core\App;
-use App\Controllers\{HomeController, AboutController};
+use App\Controllers\{HomeController, AboutController, AuthController};
 
 function registerRoutes(App $app)
 {
@@ -13,4 +13,5 @@ function registerRoutes(App $app)
     // PHP 8 可以用HomeController::class取代'App\Controllers\HomeController'
     $app->get('/', [HomeController::class, 'home']);
     $app->get('/about', [AboutController::class, 'about']);
+    $app->get('/register', [AuthController::class, 'registerView']);
 }
