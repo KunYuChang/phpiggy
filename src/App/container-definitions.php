da0aeb29808bce9dbf64102ad397d1e015456295
KunYuChang <mischiefsub@gmail.com>
2023-08-14 09:23:30 +0800

    Invoking Factory Functions

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Core\TemplateEngine;
 use App\Config\Paths;
 
-
+// The key acts as an ID for the dependency.
 return [
     TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW)
 ];
