37cf29dddce8d9f65f4e64d3e4cb4f2acf3894c1
KunYuChang <mischiefsub@gmail.com>
2023-08-14 10:25:29 +0800

    about controller 改為屬性宣告，以利使用容器依賴注入

--- src/App/Controllers/AboutController.php
+++ src/App/Controllers/AboutController.php
@@ -9,11 +9,9 @@ use App\Config\Paths;
 
 class AboutController
 {
-    private TemplateEngine $view;
-
-    public function __construct()
-    {
-        $this->view = new TemplateEngine(Paths::VIEW);
+    public function __construct(
+        private TemplateEngine $view
+    ) {
     }
 
     public function about()
