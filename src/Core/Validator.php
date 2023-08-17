3eff7b782980ea3348d1a35991de480d4ef99899
KunYuChang <mischiefsub@gmail.com>
2023-08-17 09:23:54 +0800

    Validation Rule Contract

--- src/Core/Validator.php
+++ src/Core/Validator.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace Core;
 
+use Core\Contracts\RuleInterface;
+
 class Validator
 {
+    private array $rules = [];
+
+    public function get(string $alias, RuleInterface $rule)
+    {
+        $this->rules[$alias] = $rule;
+    }
+
     public function validate(array $formData)
     {
         dd($formData);
