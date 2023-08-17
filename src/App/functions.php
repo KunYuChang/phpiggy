91418b7646cb6fd0bca53d9bf3e0ae81a913db8e
KunYuChang <mischiefsub@gmail.com>
2023-08-17 22:31:44 +0800

    Redirection with Headers

--- src/App/functions.php
+++ src/App/functions.php
@@ -16,3 +16,10 @@ function e(mixed $value): string
 {
     return htmlspecialchars((string) $value);
 }
+
+function redirect(string $path)
+{
+    header("Location: {$path}");
+    http_response_code(302);
+    exit;
+}
