964035f7989f2ad93774f0cfe0ecb63f5bd5d285
KunYuChang <mischiefsub@gmail.com>
2023-08-19 07:55:30 +0800

    Closing the Session Early

--- src/App/Middleware/SessionMiddleware.php
+++ src/App/Middleware/SessionMiddleware.php
@@ -11,16 +11,22 @@ class SessionMiddleware implements MiddlewareInterface
 {
     public function process(callable $next)
     {
+        // 檢查是否已經有一個 PHP Session 已經啟動。
         if (session_status() === PHP_SESSION_ACTIVE) {
             throw new SessionException("Session already active.");
         }
 
+        // 檢查是否已經有輸出被送到瀏覽器，以避免在 session_start() 前設定 "Set-Cookie" 標頭時可能的問題。
         if (headers_sent()) {
             throw new SessionException("Headers already sent.");
         }
 
+        // 啟動 PHP 會話，設定 "Set-Cookie" 標頭以儲存 Session 識別碼。
         session_start();
 
+        // 呼叫下一個中介軟體（或請求處理程序）以繼續處理請求。
         $next();
+
+        session_write_close();
     }
 }
