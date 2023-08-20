347046b3285fe3bc7e770f820c55c22682cde617
KunYuChang <mischiefsub@gmail.com>
2023-08-20 09:22:52 +0800

    Flashing Errors

--- src/App/Middleware/FlashMiddleware.php
+++ src/App/Middleware/FlashMiddleware.php
@@ -17,6 +17,7 @@ class FlashMiddleware implements MiddlewareInterface
     public function process(callable $next)
     {
         $this->view->addGlobal('errors', $_SESSION['errors'] ?? []);
+        unset($_SESSION['errors']);
         $next();
     }
 }
